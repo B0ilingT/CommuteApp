3668898e6552dceac0bd19cd64fe296c8190ebec
Tom <121681434+B0ilingT@users.noreply.github.com>
2024-11-03 10:49:52 +0000

    Implemented sorting

--- CommuteApp/MainWindow.xaml.cs
+++ CommuteApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using CommuteApp.Data.Clients;
 using CommuteApp.Data.Interfaces;
 using CommuteApp.ViewModels;
+using System.ComponentModel;
 using System.Net.Http;
 using System.Text;
 using System.Windows;
@@ -20,10 +21,39 @@ namespace CommuteApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GridViewColumnHeader _lastHeaderClicked;
+        private ListSortDirection _lastDirection = ListSortDirection.Ascending;
 
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            var headerClicked = sender as GridViewColumnHeader;
+            if (headerClicked == null) return;
+
+            string sortBy = headerClicked.Column.DisplayMemberBinding is Binding binding ? binding.Path.Path : null;
+            if (string.IsNullOrEmpty(sortBy)) return;
+
+            ListSortDirection direction = ListSortDirection.Ascending;
+            if (headerClicked == _lastHeaderClicked && _lastDirection == ListSortDirection.Ascending)
+                direction = ListSortDirection.Descending;
+
+            Sort(sortBy, direction);
+
+            _lastHeaderClicked = headerClicked;
+            _lastDirection = direction;
+        }
+
+        private void Sort(string sortBy, ListSortDirection direction)
+        {
+            ICollectionView dataView = CollectionViewSource.GetDefaultView(StationsListView.ItemsSource);
+
+            dataView.SortDescriptions.Clear();
+            dataView.SortDescriptions.Add(new SortDescription(sortBy, direction));
+            dataView.Refresh();
         }
     }
 }
